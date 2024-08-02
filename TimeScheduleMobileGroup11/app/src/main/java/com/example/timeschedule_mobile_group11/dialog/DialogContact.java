0ffac899d35055f7501ce4d572a78053f07a69bb
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-08-02 15:24:23 +0700

    done design home page and contact page
    Add home page in navigation, create contact layout and change icon

---
+++ TimeScheduleMobileGroup11/app/src/main/java/com/example/timeschedule_mobile_group11/dialog/DialogContact.java
@@ -0,0 +1,20 @@
+package com.example.timeschedule_mobile_group11.dialog;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.timeschedule_mobile_group11.R;
+
+public class DialogContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_contact);
+    }
+}
