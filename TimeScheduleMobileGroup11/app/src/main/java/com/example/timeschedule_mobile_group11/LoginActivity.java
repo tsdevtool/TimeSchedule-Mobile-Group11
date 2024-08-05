08bc0a9577d372ea654b678d311849334fc1cf0d
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-08-06 01:18:05 +0700

    Done login and register with email

--- TimeScheduleMobileGroup11/app/src/main/java/com/example/timeschedule_mobile_group11/LoginActivity.java
+++ TimeScheduleMobileGroup11/app/src/main/java/com/example/timeschedule_mobile_group11/LoginActivity.java
@@ -4,21 +4,29 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.firebase.Firebase;
 import com.example.timeschedule_mobile_group11.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
     AloadingDialog loading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,36 +35,70 @@ public class LoginActivity extends AppCompatActivity {
 
         loading = new AloadingDialog(this);
 
+
+        //Xu ly firebase
+        Firebase.loadFirebase();
+
+        //Xu ly dang nhap
+        addEvents();
+    }
+
+    private void addEvents() {
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent myIntent =  new Intent(LoginActivity.this, MainActivity.class);
-
-                loading.show();
-                Handler handler= new Handler();
-                Runnable runnable= new Runnable() {
-                    @Override
-                    public void run() {
-                        loading.cancel();
-                        startActivity(myIntent);
-                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
-
-                    }
-                };
-                handler.postDelayed(runnable,2000);
-
+            public void onClick(View view) {
+                //Xu ly dang nhap
+                login();
 
             }
         });
-
-        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+        binding.btnForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent myIntent =  new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(myIntent);
             }
         });
+    }
 
+    private void login() {
+        String email, password;
+        email = binding.edtUserCode.getText().toString();
+        password = binding.edtPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email) ){
+            Toast.makeText(this, "Vui lòng nhập email hoặc mã số của bạn!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password) ){
+            Toast.makeText(this, "Vui lòng nhập mật khẩu!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Firebase.mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    //Hiển thị loading khi chuyển trang
+                    loading.show();
+                    Handler handler= new Handler();
+                    Runnable runnable= new Runnable() {
+                        @Override
+                        public void run() {
+                            loading.cancel();
+                            startActivity(intent);
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+
+                        }
+                    };
+                    handler.postDelayed(runnable,2000);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Tên đăng nhập hoặc mật khẩu sai!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
--- TimeScheduleMobileGroup11/app/src/main/java/com/example/timeschedule_mobile_group11/RegisterActivity.java
+++ TimeScheduleMobileGroup11/app/src/main/java/com/example/timeschedule_mobile_group11/RegisterActivity.java
@@ -4,17 +4,24 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.firebase.Firebase;
 import com.example.timeschedule_mobile_group11.databinding.ActivityRegisterBinding;
+import com.example.utils.Password;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -32,6 +39,62 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         loading = new AloadingDialog(this);
+
+//        Xu ly thoi gian
+        TimeHandling();
+
+//        Xu ly cac button
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btnRegisterSucess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email, password;
+                email = binding.edtUsername.getText().toString();
+                password = Password.generatePassword(12);
+
+                if(TextUtils.isEmpty(email) ){
+                    Toast.makeText(RegisterActivity.this, "Vui lòng nhập email hoặc mã số của bạn!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password) ){
+                    Toast.makeText(RegisterActivity.this, "Vui lòng nhập mật khẩu!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Firebase.mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent myIntent =  new Intent(RegisterActivity.this, LoginActivity.class);
+                            loading.show();
+                            Handler handler= new Handler();
+                            Runnable runnable= new Runnable() {
+                                @Override
+                                public void run() {
+                                    loading.cancel();
+                                    startActivity(myIntent);
+                                    Toast.makeText(RegisterActivity.this, "Tạo tài khoản thành công!", Toast.LENGTH_SHORT).show();
+
+                                }
+                            };
+                            handler.postDelayed(runnable,2000);
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Tạo tài khoản không thành công!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+        });
+    }
+
+    private void TimeHandling() {
         calendar= Calendar.getInstance();
         DatePickerDialog.OnDateSetListener date= new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -48,26 +111,6 @@ public class RegisterActivity extends AppCompatActivity {
         binding.edtDate.setOnClickListener(view->{
             new DatePickerDialog(RegisterActivity.this,date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
         });
-
-
-        binding.btnRegisterSucess.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent =  new Intent(RegisterActivity.this, LoginActivity.class);
-                loading.show();
-                Handler handler= new Handler();
-                Runnable runnable= new Runnable() {
-                    @Override
-                    public void run() {
-                        loading.cancel();
-                        startActivity(myIntent);
-                        Toast.makeText(RegisterActivity.this, "Tạo tài khoản thành công!", Toast.LENGTH_SHORT).show();
-
-                    }
-                };
-                handler.postDelayed(runnable,2000);
-            }
-        });
     }
 
     private void updatelabel() {
@@ -76,4 +119,5 @@ public class RegisterActivity extends AppCompatActivity {
         SimpleDateFormat dateFormat = new SimpleDateFormat( myFormat, Locale.US);
         binding.edtDate.setText(dateFormat.format(calendar.getTime()));
     }
+
 }
