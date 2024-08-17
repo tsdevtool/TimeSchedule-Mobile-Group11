782cc2d17a7c9be5a858ecb65daf0970a54c482a
quanleminh2003 <lequan14803@gmail.com>
2024-08-18 04:51:37 +0700

    done
    xong giao diện , chức năng của trang thông tin người dùng

--- TimeScheduleMobileGroup11/app/src/main/java/com/example/timeschedule_mobile_group11/fragment/UserFragment.java
+++ TimeScheduleMobileGroup11/app/src/main/java/com/example/timeschedule_mobile_group11/fragment/UserFragment.java
@@ -1,25 +1,31 @@
 package com.example.timeschedule_mobile_group11.fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.example.timeschedule_mobile_group11.LoginActivity;
 import com.example.timeschedule_mobile_group11.R;
 import com.example.timeschedule_mobile_group11.databinding.ActivityUserFragmentBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -66,6 +72,15 @@ public class UserFragment extends AppCompatActivity {
                 String dayOfBirth = dataSnapshot.child("dayOfBirth").getValue(String.class);
                 String dayAdmission = dataSnapshot.child("dayAdmission").getValue(String.class);
                 String photoUrl = dataSnapshot.child("photoUrl").getValue(String.class);
+                // Lấy giá trị của trường sex (giới tính)
+                Boolean sex = dataSnapshot.child("sex").getValue(Boolean.class);
+                String sexFormatted;
+                if (sex != null && sex) {
+                    sexFormatted = "Nam"; // true -> Nam
+                } else {
+                    sexFormatted = "Nữ"; // false -> Nữ
+                }
+
                 // Kiểm tra và hiển thị ảnh đại diện nếu có
                 if (photoUrl != null) {
                     Glide.with(UserFragment.this)
@@ -82,6 +97,7 @@ public class UserFragment extends AppCompatActivity {
                 binding.txtEmail.setText(email);
                 binding.txtdayOfBirth.setText(dayOfBirth);
                 binding.txtdayAdmission.setText(dayAdmission);
+                binding.txtSex.setText(sexFormatted);
             }
 
             @Override
@@ -92,7 +108,7 @@ public class UserFragment extends AppCompatActivity {
         });
 
         // Xử lý sự kiện khi nhấn vào nút Đổi Avatar
-        binding.btnChangeAvatar.setOnClickListener(new View.OnClickListener() {
+        binding.imvAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 chooseImage();
@@ -102,12 +118,103 @@ public class UserFragment extends AppCompatActivity {
         binding.btnChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newPassword = binding.txtNewPassword.getText().toString();
-                if (!TextUtils.isEmpty(newPassword)) {
-                    changePassword(newPassword);
-                } else {
-                    Toast.makeText(UserFragment.this, "Please enter a new password", Toast.LENGTH_SHORT).show();
+                String currentPassword = binding.txtCurrentPassword.getText().toString().trim(); // Người dùng nhập mật khẩu hiện tại
+                String newPassword = binding.txtNewPassword.getText().toString().trim(); // Người dùng nhập mật khẩu mới
+
+                if (TextUtils.isEmpty(currentPassword)) {
+                    // Hiển thị thông báo nếu mật khẩu cũ trống
+                    new AlertDialog.Builder(UserFragment.this)
+                            .setTitle("Lỗi")
+                            .setMessage("Vui lòng nhập mật khẩu hiện tại.")
+                            .setPositiveButton(android.R.string.ok, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                    return; // Dừng lại nếu không có mật khẩu cũ
                 }
+
+                if (TextUtils.isEmpty(newPassword)) {
+                    // Hiển thị thông báo nếu mật khẩu mới trống
+                    new AlertDialog.Builder(UserFragment.this)
+                            .setTitle("Lỗi")
+                            .setMessage("Vui lòng nhập mật khẩu mới.")
+                            .setPositiveButton(android.R.string.ok, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                    return; // Dừng lại nếu không có mật khẩu mới
+                }
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), currentPassword);
+
+                    // Xác thực lại người dùng
+                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                // Xác thực thành công, tiến hành đổi mật khẩu
+                                changePassword(newPassword);
+                            } else {
+                                // Xác thực thất bại, thông báo lỗi
+                                new AlertDialog.Builder(UserFragment.this)
+                                        .setTitle("Lỗi")
+                                        .setMessage("Mật khẩu hiện tại không chính xác. Vui lòng thử lại.")
+                                        .setPositiveButton(android.R.string.ok, null)
+                                        .setIcon(android.R.drawable.ic_dialog_alert)
+                                        .show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+// Xử lý sự kiện khi nhấn vào nút Show/Hide mật khẩu cũ
+        binding.btnShowCurrentPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.txtCurrentPassword.getInputType() == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)) {
+                    // Hiển thị mật khẩu
+                    binding.txtCurrentPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    binding.btnShowCurrentPassword.setImageResource(R.drawable.eye_26); // Biểu tượng mở khóa
+                } else {
+                    // Ẩn mật khẩu
+                    binding.txtCurrentPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    binding.btnShowCurrentPassword.setImageResource(R.drawable.invisible_26); // Biểu tượng khóa
+                }
+                // Đưa con trỏ về cuối đoạn văn bản
+                binding.txtCurrentPassword.setSelection(binding.txtCurrentPassword.getText().length());
+            }
+        });
+        // Xử lý sự kiện khi nhấn vào nút hiển thị mật khẩu
+        binding.btnShowPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.txtNewPassword.getInputType() == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)) {
+                    // Hiển thị mật khẩu
+                    binding.txtNewPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    binding.btnShowPassword.setImageResource(R.drawable.eye_26); // Biểu tượng mở khóa
+                } else {
+                    // Ẩn mật khẩu
+                    binding.txtNewPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    binding.btnShowPassword.setImageResource(R.drawable.invisible_26); // Biểu tượng khóa
+                }
+                // Đưa con trỏ về cuối đoạn văn bản
+                binding.txtNewPassword.setSelection(binding.txtNewPassword.getText().length());
+            }
+        });
+        //Xử lý đăng xuất tài khoản
+        binding.btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Đăng xuất người dùng khỏi Firebase
+                FirebaseAuth.getInstance().signOut();
+
+                // Điều hướng người dùng về màn hình đăng nhập (hoặc bất kỳ màn hình nào bạn muốn)
+                Intent intent = new Intent(UserFragment.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Xóa tất cả các activity trước đó khỏi stack
+                startActivity(intent);
+                finish(); // Kết thúc activity hiện tại
             }
         });
     }
@@ -121,15 +228,38 @@ public class UserFragment extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
-                                Toast.makeText(UserFragment.this, "Password updated successfully", Toast.LENGTH_SHORT).show();
+                                // Hiển thị hộp thoại thông báo thành công
+                                new AlertDialog.Builder(UserFragment.this)
+                                        .setTitle("Success")
+                                        .setMessage("Mật khẩu của bạn đã được thay đổi thành công.")
+                                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                // Trả người dùng về màn hình đăng nhập hoặc thực hiện một hành động khác
+                                                dialog.dismiss(); // Đóng dialog
+                                            }
+                                        })
+                                        .setIcon(R.drawable.success)
+                                        .show();
                             } else {
                                 Log.e("Firebase", "Failed to update password", task.getException());
-                                Toast.makeText(UserFragment.this, "Failed to update password", Toast.LENGTH_SHORT).show();
+                                // Hiển thị hộp thoại thông báo lỗi
+                                new AlertDialog.Builder(UserFragment.this)
+                                        .setTitle("Error")
+                                        .setMessage("Có lỗi xảy ra khi đổi mật khẩu. Vui lòng thử lại.")
+                                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss(); // Đóng dialog
+                                            }
+                                        })
+                                        .setIcon(android.R.drawable.ic_dialog_info)
+                                        .show();
                             }
                         }
                     });
         }
     }
+
+
 
     private void chooseImage() {
         Intent intent = new Intent();
@@ -158,52 +288,69 @@ public class UserFragment extends AppCompatActivity {
         }
     }
 
+
     private void uploadImageToFirebase(Uri filePath) {
-        if (filePath != null) {
-            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            StorageReference ref = storageReference.child("avatars/" + userId);
+    if (filePath != null) {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        StorageReference ref = storageReference.child("avatars/" + userId);
 
-            ref.putFile(filePath)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            // Lấy URL của tệp đã tải lên
-                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    // URL đã lấy được
-                                    String imageUrl = uri.toString();
-                                    Log.d("Firebase", "File URL: " + imageUrl);
+        ref.putFile(filePath)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String imageUrl = uri.toString();
+                                Log.d("Firebase", "File URL: " + imageUrl);
 
-                                    // Lưu URL vào Firebase Realtime Database để hiển thị sau
-                                    saveImageUrlToDatabase(imageUrl);
+                                saveImageUrlToDatabase(imageUrl);
 
-                                    // Hiển thị hình ảnh trong ImageView
-                                    Glide.with(UserFragment.this)
-                                            .load(imageUrl)
-                                            .into(binding.imvAvatar);
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.e("Firebase", "Failed to get download URL", e);
-                                    Toast.makeText(UserFragment.this, "Failed to get download URL", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.e("Firebase", "Failed to upload avatar", e);
-                            Toast.makeText(UserFragment.this, "Failed to upload avatar", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        } else {
-            Log.e("Firebase", "File path is null");
-            Toast.makeText(UserFragment.this, "File path is null", Toast.LENGTH_SHORT).show();
-        }
+                                Glide.with(UserFragment.this)
+                                        .load(imageUrl)
+                                        .into(binding.imvAvatar);
+
+                                // Hiển thị hộp thoại thông báo thành công
+                                new AlertDialog.Builder(UserFragment.this)
+                                        .setTitle("Cập nhập ảnh đại diện")
+                                        .setMessage("Thay đổi ảnh đại diện thành công")
+                                        .setPositiveButton(android.R.string.ok, null)
+                                        .show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("Firebase", "Failed to get download URL", e);
+                                new AlertDialog.Builder(UserFragment.this)
+                                        .setTitle("Error")
+                                        .setMessage("Thay đổi ảnh đại diện thất bại.")
+                                        .setPositiveButton(android.R.string.ok, null)
+                                        .show();
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Firebase", "Failed to upload avatar", e);
+                        new AlertDialog.Builder(UserFragment.this)
+                                .setTitle("Error")
+                                .setMessage("Failed to upload avatar.")
+                                .setPositiveButton(android.R.string.ok, null)
+                                .show();
+                    }
+                });
+    } else {
+        Log.e("Firebase", "File path is null");
+        new AlertDialog.Builder(UserFragment.this)
+                .setTitle("Error")
+                .setMessage("File path is null.")
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
     }
+}
+
 
     private void saveImageUrlToDatabase(String imageUrl) {
         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
